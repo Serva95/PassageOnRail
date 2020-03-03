dda1ffb75d06323f7a4c7d3c19bc6e0373290ec3
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-03-03 15:35:14 +0100

    !!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class UserDOBValidator < ActiveModel::Validator
   def validate(record)
     if record.data_di_nascita.blank?
-      record.errors[:data_di_nascita] << "campo obbligatorio"
+      record.errors[:data_di_nascita] << "campo obbligatorio!!"
     elsif record.data_di_nascita > 18.years.ago
       record.errors[:data_di_nascita] << "non accettabile (scusa, ma devi avere almeno 18 anni per iscriverti)"
     end
