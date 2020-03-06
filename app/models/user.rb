f87bc1ddd457ee6f1105ede10094243e87056921
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2020-03-06 17:17:26 +0100

    modifiche usr

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,10 @@ end
 
 class User < ApplicationRecord
  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-	 devise :database_authenticatable, :registerable,
+ #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	 devise :registerable,
          :recoverable, :rememberable, :validatable
-	
-	validates_with UserDOBValidator
+          # :database_authenticatable
+
+  validates_with UserDOBValidator
 end
