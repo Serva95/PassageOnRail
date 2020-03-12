f7ddac91815e8c5d68d83e9782bbe2b582204876
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-03-12 16:05:47 +0100

    miglioramento ricerca

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,8 +9,8 @@ class Search < ApplicationRecord
     routes = routes.where(["data_ora_partenza >= ?", data_ora]) if data_ora.present?
     routes = Route.joins(:driver).where(["drivers.rating_medio >= ?",rating]) if rating.present?
     routes = routes.where(["costo <= ?",costo]) if costo.present?
-    #routes = Route.joins(:vehicle).where(["vehicles.marca LIKE ?","%#{tipo_mezzo}"]) if tipo_mezzo.present?
-    #routes = Route.joins(:vehicle).where(["vehicles.comfort >= ?",comfort]) if comfort.present?
+    routes = Route.joins(:vehicle).where(["vehicles.marca LIKE ?","%#{tipo_mezzo}"]) if tipo_mezzo.present?
+    routes = Route.joins(:vehicle).where(["vehicles.comfort >= ?",comfort]) if comfort.present?
 
     return routes
 
