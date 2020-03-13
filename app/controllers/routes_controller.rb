9094a0a7a89f190083f0be88f9836026fa8de3a9
martina <martina.proietti@student.unife.it>
2020-03-12 18:06:10 -0600

    Revert "cambiamento di prova"
    This reverts commit 91808a629929dc915982c2c9f5210417187d1aa4.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -8,8 +8,6 @@ class RoutesController < ApplicationController
     @routes = Route.search(params[:search])
   end
 
-  # GET drivers/:id_driver/routes/journey
-  # GET drivers/:id_driver/routes/journey
   def journey
     @routes = @driver.routes
   end
@@ -25,16 +23,15 @@ class RoutesController < ApplicationController
     @route = @driver.routes.build
   end
 
-  # GET driver/:id_driver/routes/1/edit
+  # GET /routes/1/edit
   def edit
   end
 
-  # POST driver/:id_driver/routes
-  # POST driver/:id_driver/routes.json
+  # POST /routes
+  # POST /routes.json
   def create
     #@driver = Driver.vehicles.find([:vehicles_id])
 	  @route = @driver.routes.build(route_params)
-    @route.n_passeggeri = 0
     #@route.vehicle = Vehicle.find_by_id(1)
 
     respond_to do |format|
@@ -48,8 +45,8 @@ class RoutesController < ApplicationController
     end
   end
 
-  # PATCH/PUT driver/:id_driver/routes/1
-  # PATCH/PUT /routes//1.json
+  # PATCH/PUT /routes/1
+  # PATCH/PUT /routes/1.json
   def update
     respond_to do |format|
       if @route.update(route_params)
