7e1a1cea21fecd2f06609f2f5e6bb95bfc99d502
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-03-13 22:09:48 +0100

    primi tentativi ordinamento

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,9 @@
 class Search < ApplicationRecord
 
+  def define_order(s_order)
+    s_order='DECRESCENTE' ? :asc : :desc
+  end
+
   def search_routes
 
     routes = Route.all
@@ -11,6 +15,10 @@ class Search < ApplicationRecord
     routes = routes.where(["costo <= ?",costo]) if costo.present?
     routes = Route.joins(:vehicle).where(["vehicles.marca LIKE ?","%#{tipo_mezzo}"]) if tipo_mezzo.present?
     routes = Route.joins(:vehicle).where(["vehicles.comfort >= ?",comfort]) if comfort.present?
+
+    routes = routes.order(:costo) if sort_attribute='Costo'
+    routes = routes.order(:tempo_percorrenza) if sort_attribute='Tempo di percorrenza'
+
 
     return routes
 
