0d3709426cb0d5025c3500f844f617e471cf6e17
martina <martina.proietti@student.unife.it>
2020-03-16 11:23:40 +0100

    Aggiunte colonne alla tabella searches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_112715) do
+ActiveRecord::Schema.define(version: 2020_03_13_202922) do
 
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -128,6 +128,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_112715) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "comfort"
+    t.string "sort_attribute"
+    t.string "sort_order"
   end
 
   create_table "users", force: :cascade do |t|
