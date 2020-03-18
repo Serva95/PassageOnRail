e07b4629b7e4366fd2fd6747936e1fc4d1389728
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-03-18 11:00:07 +0100

    Aggiornamento multi search

--- app/models/search.rb
+++ app/models/search.rb
@@ -30,7 +30,7 @@ class Search < ApplicationRecord
 
     #tutto ciò che è commentato sono tentativi disperati o bozze di idee...COME FACCIO A TIRAR FUORI DUE ROUTE ACCOPPIATE????
     #select_clause1 = 'routes.*'
-    select_clause1= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, other_routes.id AS id2, other_routes.citta_arrivo AS c_arr, SUM(routes.costo+other_routes.costo) AS c_tot'
+    select_clause1= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, other_routes.id AS id2, other_routes.citta_arrivo AS c_arr, SUM(routes.costo+other_routes.costo) AS c_tot, (SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza)/60) AS ore,(SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza)%60) AS min'
     select_clause2 = 'routes.*, other_routes.*'
     from_clause = 'routes, routes as other_routes'
     where_clause = "routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ?"
