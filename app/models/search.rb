60168a7d461d60c1cb92eb1d7d491df4dfaa3c82
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-03-18 11:18:31 +0100

    miglioramento search multi tratta

--- app/models/search.rb
+++ app/models/search.rb
@@ -28,29 +28,21 @@ class Search < ApplicationRecord
 
   def multi_routes_search
 
-    #tutto ciò che è commentato sono tentativi disperati o bozze di idee...COME FACCIO A TIRAR FUORI DUE ROUTE ACCOPPIATE????
-    #select_clause1 = 'routes.*'
-    select_clause1= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, other_routes.id AS id2, other_routes.citta_arrivo AS c_arr, SUM(routes.costo+other_routes.costo) AS c_tot, (SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza)/60) AS ore,(SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza)%60) AS min'
-    select_clause2 = 'routes.*, other_routes.*'
+    select_clause= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, other_routes.id AS id2, other_routes.citta_arrivo AS c_arr,
+                    SUM(routes.costo+other_routes.costo) AS c_tot, SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza) AS t_tot, (SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza)/60) AS ore,(SUM(routes.tempo_percorrenza+other_routes.tempo_percorrenza)%60) AS min'
     from_clause = 'routes, routes as other_routes'
     where_clause = "routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ?"
     group_clause = 'routes.id,other_routes.id'
 
-      routes=Route.all
-      routes1 =routes.select(select_clause1).where([where_clause,"%#{c_partenza}","%#{c_arrivo}"]).from(from_clause).group(group_clause)
+    routes=Route.all
+    routes =routes.select(select_clause).where([where_clause,"%#{c_partenza}","%#{c_arrivo}"]).from(from_clause).group(group_clause)
 
-   # routes2=Route.select(select_clause2).where([where_clause,"%#{c_partenza}","%#{c_arrivo}"]).from(from_clause)
-    # routes=Route.select(select_clause1).where([routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ? AND routes.id=?","%#{c_partenza}","%#{c_arrivo}",routes1.id]).from(from_clause)
+    sorder=define_order(sort_order)
 
+    routes = routes.order(c_tot: sorder) if sort_attribute.eql?('Costo')
+    routes = routes.order(t_tot: sorder) if sort_attribute.eql?('Tempo di percorrenza')
 
-    #$routeprima=routes1.ids #PARE che ci metta dentro gli id di tutte le prime tratte -> provare a sfruttarla per tirare fuori coppie di id
-    #$routeprima.each do |rp|
-    #  routes=Route.select(select_clause1).where([routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ? AND routes.id=?","%#{c_partenza}","%#{c_arrivo}",rp]).from(from_clause)
-     #MAGARI SE LO METTO IN UN'ALTRA FUNZIONE FUNZIONA (DEVO AVERE LA CERTEZZA DI POTERGLI PASSARE routeprima)
-
-
-    #routes1=Route.find_by_id(routes1.id) però come fare se routes1 ne tira fuori di più?
-    return routes1 #, routes2
+    return routes
 
   end
 
