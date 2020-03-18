8e46be066b3b0582f0b9b440cda8633f85129874
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-03-18 10:29:24 +0100

    miglioramento search multitratta

--- app/models/search.rb
+++ app/models/search.rb
@@ -30,13 +30,14 @@ class Search < ApplicationRecord
 
     #tutto ciò che è commentato sono tentativi disperati o bozze di idee...COME FACCIO A TIRAR FUORI DUE ROUTE ACCOPPIATE????
     #select_clause1 = 'routes.*'
-    select_clause1= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, other_routes.id AS id2, other_routes.citta_arrivo AS c_arr'
+    select_clause1= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, other_routes.id AS id2, other_routes.citta_arrivo AS c_arr, SUM(routes.costo+other_routes.costo) AS c_tot'
     select_clause2 = 'routes.*, other_routes.*'
     from_clause = 'routes, routes as other_routes'
     where_clause = "routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ?"
+    group_clause = 'routes.id,other_routes.id'
 
       routes=Route.all
-      routes1 =routes.select(select_clause1).where([where_clause,"%#{c_partenza}","%#{c_arrivo}"]).from(from_clause)
+      routes1 =routes.select(select_clause1).where([where_clause,"%#{c_partenza}","%#{c_arrivo}"]).from(from_clause).group(group_clause)
 
    # routes2=Route.select(select_clause2).where([where_clause,"%#{c_partenza}","%#{c_arrivo}"]).from(from_clause)
     # routes=Route.select(select_clause1).where([routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ? AND routes.id=?","%#{c_partenza}","%#{c_arrivo}",routes1.id]).from(from_clause)
