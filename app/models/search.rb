4a2171a36295d856f0b3e28a3de11b52eabad448
Serva <43950391+Serva95@users.noreply.github.com>
2020-03-20 12:17:55 +0100

    multi tratta per PG

--- app/models/search.rb
+++ app/models/search.rb
@@ -42,11 +42,11 @@ class Search < ApplicationRecord
 
     select_clause= 'routes.id AS id1, routes.citta_partenza AS c_part, routes.data_ora_partenza AS part, routes.citta_arrivo AS tappa, routes.vehicle_id,
                     other_routes.id AS id2, other_routes.citta_arrivo AS c_arr,
-                    SUM(routes.costo+other_routes.costo) AS c_tot, (DATE_PART(''DAY'', routes.data_ora_partenza - routes.data_ora_arrivo) * 24 +
-                                                                   DATE_PART(''HOUR'',routes.data_ora_partenza - routes.data_ora_arrivo)) AS ore,
-                                                                  ((DATE_PART(''DAY'', routes.data_ora_partenza - routes.data_ora_arrivo) * 24 +
-                                                                  DATE_PART(''HOUR'',routes.data_ora_partenza - routes.data_ora_arrivo))*60)+
-                                                                  DATE_PART(''MINUTE'', routes.data_ora_partenza - routes.data_ora_arrivo)AS min'
+                    SUM(routes.costo+other_routes.costo) AS c_tot, (EXTRACT(DAY FROM routes.data_ora_partenza - routes.data_ora_arrivo) * 24 +
+                                                                   EXTRACT(HOUR FROM routes.data_ora_partenza - routes.data_ora_arrivo)) AS ore,
+                                                                  ((EXTRACT(DAY FROM routes.data_ora_partenza - routes.data_ora_arrivo) * 24 +
+                                                                  EXTRACT(HOUR FROM routes.data_ora_partenza - routes.data_ora_arrivo))*60)+
+                                                                  EXTRACT(MINUTE FROM routes.data_ora_partenza - routes.data_ora_arrivo)AS min'
     from_clause = 'routes, routes as other_routes'
     where_clause = "routes.citta_arrivo = other_routes.citta_partenza AND routes.citta_partenza LIKE ? AND other_routes.citta_arrivo LIKE ? AND other_routes.data_ora_partenza >= routes.data_ora_arrivo"
     group_clause = 'routes.id,other_routes.id'
