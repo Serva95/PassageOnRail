a1e5f616dc829a9fb8e8b45d7f404b655890babf
martina <martina.proietti@student.unife.it>
2020-03-25 19:03:53 +0100

    tratta prenotabile solo una volta

--- app/models/passenger_association.rb
+++ app/models/passenger_association.rb
@@ -1,4 +1,12 @@
 class PassengerAssociation < ApplicationRecord
 	belongs_to :route, dependent: :destroy
 	belongs_to :hitch_hiker, dependent: :destroy
+
+	validates :n_prenotati, presence: true
+
+	# con queste due righe dovrebbe impedire ad un utente
+	# di prenotare tratte già prenotate in precedenza
+	add_index :passenger_association, [:route_id, :hitch_hiker_id], unique: true
+	validates_uniqueness_of :route_id, scope: [:hitch_hiker_id]
+
 end
