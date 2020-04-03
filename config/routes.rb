010a8d610b11eccc8827f6cfed372028274988f3
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2020-04-03 11:21:53 +0200

    Fix error route sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   devise_scope :user do
-    get '/user/sign_out', to: 'users/sessions#destroy'
+    get '/user/sign_out', to: 'devise/sessions#destroy'
+    get '/users/:id/bookings', to: 'users#bookings', as: 'user_bookings'
   end
   resources :passenger_associations
   resources :drivers do
@@ -31,5 +32,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
-  get '/users/:id/bookings', to: 'users#bookings', as: 'user_bookings'
 end
