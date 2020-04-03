70f7360142460c435c9a656305b7b48af9c1b7e3
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2020-04-03 11:43:02 +0200

    Fix error route sign-out pt.3 posso farcela

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users/registrations'}
+  devise_for :users, controllers: {sessions:'users/sessions', registrations: 'users/registrations'}
   devise_scope :user do
     get '/user/sign_out', to: 'users/sessions#destroy'
   end
