84ee7a82bc8d21436072438c396f928806c24f63
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2020-04-11 17:36:49 +0200

    data validation metodo di pagamento

--- app/models/pay_methods.rb
+++ app/models/pay_methods.rb
@@ -1,17 +1,20 @@
 class PayMethodsDOBValidator < ActiveModel::Validator
   def validate(record)
-    if record.intestatario.blank? or record.numero.blank?  or record.mese.blank? or record.anno.blank? or record.cvv.blank?
-      record.errors[:testo] << "Devi compilare tutti i campi!"
+    anno = record.anno_scadenza + 2000
+    if anno == Time.new.year and record.mese_scadenza < Time.new.month
+      record.errors[:error] << "Data di scadenza non valida"
     end
   end
 end
 
 class PayMethods < ApplicationRecord
   validates :intestatario, presence: true
-  validates :numero, presence: true
-  validates :mese_scadenza, presence: true
-  validates :anno_scadenza, presence: true
-  validates :cvv, presence: true
+  validates :numero, presence: true, format: { with: /\A[0-9]+\z/, message: "only allows number" }, length: { is: 16 }
+  validates :mese_scadenza, presence: true, numericality: { only_integer: true }, inclusion: {in: 1..12}
+  validates :anno_scadenza, presence: true, numericality: { only_integer: true }, inclusion: {in: Time.new.year-2000..(Time.new.year-1990)}
+  validates :cvv, presence: true, format: { with: /\A[0-9]+\z/, message: "only allows number" }, length: { is: 3 }
+
+  validates_with PayMethodsDOBValidator
 
   belongs_to :user
 end
