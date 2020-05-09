334d1410a12b51e9f248d2435d58462d2f9e47f8
alicebizzarri <alice.bizzarri@student.unife.it>
2020-05-09 14:05:14 +0200

    aggiunta pagina errore 500 e sistemato layout pagina recupero password e email di conferma

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,19 @@
 class ErrorsController < ApplicationController
-
-
+  
   def not_found
-    raise ActionController::RoutingError.new('Not Found')
-  rescue
-  # ignored
+  #   raise ActionController::RoutingError.new('Not Found')
+  # rescue
+  # # ignored
   end
 
   def record_not_found
-    raise ActiveRecord::RecordNotFound
-  rescue
-    # ignored
+
   end
 
+  def internal_server_error
+  #   raise ActionView::Template::Error
+  # rescue
+  #   # ignored
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {sessions:'users/sessions', registrations: 'users/registrations'}
+  devise_for :users, controllers: {sessions:'users/sessions', registrations: 'users/registrations',
+                                   passwords:'users/passwords', confirmations: 'users/confirmations'}
   devise_scope :user do
     get '/user/sign_out', to: 'users/sessions#destroy'
     get '/users/:id/bookings', to: 'users#bookings', as: 'user_bookings'
@@ -46,6 +47,7 @@ Rails.application.routes.draw do
   end
   get "/404", :to => "errors#not_found"
   get "/442", :to => "errors#record_not_found"
+  get "/500", :to => "errors#internal_server_error"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
   get '/users/:id/pay_methods/', to: 'pay_method#index',  as: 'user_pay_methods_index'
