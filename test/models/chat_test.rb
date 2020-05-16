b847f06cacf2ae9303290b5b72e6ef8205d1c2e5
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2020-05-16 12:36:19 +0200

    inizio sviluppo test (non provati)

--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class ChatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "#find_chats(user_1) should return all chats of user_1" do
+    c1 = chats(:one)
+    c2 = chats(:two)
+    assert_includes Chat.find_chats(1), c1
+    assert_includes Chat.find_chats(1), c2
+  end
+
+  test "#exists(user_1,user_2) should return the chat if exists" do
+    c = chats(:two)
+    assert_includes Chat.exists(1,2), c
+  end
+
+  test "#delete_reset_side(chat, 1, true) should delete the chat for the user_1" do
+    c = chats(:to_be_deleted)
+    c.delete_reset_side(c,1,true)
+    assert_equal "true", Chats.find(chats(:to_be_deleted)).deleted_user_1
+  end
+
+  test "#delete_reset_side(chat, 2, false) should reset the chat for the user_1" do
+    c = chats(:to_be_restored)
+    c.delete_reset_side(c,2,false)
+    assert_equal "true", Chats.find(chats(:to_be_deleted)).deleted_user_2
+  end
+
 end
--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "#max_passeggeri should return vehicle.posti" do
+    v = vehicles(:has_4_seats)
+    assert_equal v.max_passeggeri(1), 4
+  end
+
+  test "#estrai_posti should return vehicle.posti" do
+    v = vehicles(:has_4_seats)
+    assert_equal v.estrai_posti(1), 4
+  end
+
 end
