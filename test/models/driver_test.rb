26e8d74e7cd4924078c6a03a40ebd54439a5a35a
alicebizzarri <alice.bizzarri@student.unife.it>
2020-05-17 18:00:18 +0200

    Fix file .yml test and make test driver

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,5 +1,10 @@
 require 'test_helper'
 
 class DriverTest < ActiveSupport::TestCase
-  test "#has_routes(id) should return if driver has route"
+  test "#has_routes(id) should return if driver has route" do
+    d1 = drivers(:with_route)
+    d2 = drivers(:no_route)
+    assert d1.has_routes(d1.id)
+    assert_not d2.has_routes(d2.id)
+  end
 end
