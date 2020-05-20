f4d7aa7f622719adef98abfa41cafe682415e792
Serva <43950391+Serva95@users.noreply.github.com>
2020-05-20 13:24:16 +0200

    review_test.rb completo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ require "minitest/rails"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: :number_of_processors,  with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
