e5ca3032ba9881b92c0ce5f9c462de7acf63ff0b
Serva <43950391+Serva95@users.noreply.github.com>
2020-09-03 22:55:24 +0200

    integration test: ajax per notifiche chat e flusso creazione chat

---
+++ test/integration/chat_creation_flow_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+describe "Chat creation flow", :integration do
+  test "chat_creation_flow" do
+    get "/"
+    assert_response :redirect
+    follow_redirect!
+    assert_select "h2", "Log in"
+
+    post "/users/sign_in",
+         params: {user: {email: "lorenzo@test.it", password: "password", }}
+    assert_response :redirect
+    follow_redirect!
+    assert_equal 200, status
+    assert_equal "/", path
+
+    get "/users/8/bookings"
+    assert_equal 200, status
+
+    get "/users/8/bookings/detail?j_id=6"
+    assert_equal 200, status
+
+    post "/chats",
+         params: {chat: {user_2_id: "1"}}
+    assert_response :redirect
+    follow_redirect!
+    assert_equal 200, status
+    assert_select "h3", "Chat con Nome Cognome"
+  end
+end
