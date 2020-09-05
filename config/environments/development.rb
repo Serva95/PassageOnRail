403103fa536ace2c84e1a030f2a2a271de2f1c58
Serva <43950391+Serva95@users.noreply.github.com>
2020-09-06 00:01:43 +0200

    fix registrazione utente + gem update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,14 +38,14 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.default_url_options = { host: 'isneyt2ttxnhxzew.myfritz.net:50080'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       user_name:      'passageonrails@gmail.com',
       password:       'MaledettoCovid-19',
-      domain:         'localhost:3000',
-      address:       'smtp.gmail.com',
-      port:          '587',
+      domain:         'localhost:80',
+      address:        'smtp.gmail.com',
+      port:           '587',
       authentication: :plain
   }
   config.action_mailer.perform_caching = false
