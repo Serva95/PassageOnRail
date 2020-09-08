006c17fcea2b8533a429da4c8c074bee9ec80c93
Martina <martinaproietti42@gmail.com>
2020-09-08 17:16:49 +0200

    deleted file

--- test/integration/reservation_flow_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-describe "Reservation flow", :integration do
-  test "user reservation" do
-  user = users(:four)
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
-end
